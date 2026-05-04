01440c6524ddcd718d801a8346edb6567a1fbf3e
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-04 19:17:19 +1200

    4hr 30m made the power of two equasion, also fixing ai chemical mass

---
+++ physics/Maths.py
@@ -0,0 +1,17 @@
+d = 'p'
+a = 0
+b = 0
+c = 0
+while d != '0':
+    try:
+        a = float(input('a: '))
+        b = float(input('b: '))
+        c = float(input('c: '))
+        if ((b**2) - (4*a*b)) >= 0:
+            print((-(b)-(((b**2)-(4*a*c))**0.5))/(2*a))
+            print((-(b)+(((b**2)-(4*a*c))**0.5))/(2*a))
+        else:
+            print('No True')
+    except:
+        print('')
+    d = input('cont ! 0')
